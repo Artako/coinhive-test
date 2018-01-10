bd3ab182c7977a52efffcaaba6eac1b3c52db61f
Artako <31965199+Artako@users.noreply.github.com>
2018-01-10 18:31:06 +0400

    Add files via upload

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './app';
+class Root extends React.Component {
+  render() {
+    return (
+      <div>
+        <App />
+      </div>
+    );
+  }
+}
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import './index.pcss';
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
